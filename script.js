c6ad99c1afc742610f62ae55e4e230c9b38ab3fa
Michael Parenti <78945650+M-Coder-3920@users.noreply.github.com>
2023-03-23 11:52:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
+//Declarations
 var nav = document.querySelector('nav');
 var nav_img = document.getElementById('img');
 var nav_menu = document.getElementById('hamburger');
+
+var full = document.getElementById('fullscreen-menu');
+var close = document.getElementById('close');
+var home = document.getElementById('home');
+var projects = document.getElementById('projects');
+var about = document.getElementById('about');
+var contact = document.getElementById('contact');
+
 var card = document.getElementById('info-cards');
 var form = document.getElementById('form');
+
+
+
+////////////////////////////////
+
+//Fullscreen Menu Controls
+nav_menu.addEventListener('click', () => {
+    full.style.animation = 'slide-in ease 1s';
+    setTimeout(() => {
+        full.style.transform = 'translateX(0%)';
+    }, 100)
+});
+
+close.addEventListener('click', () => {
+    full.style.animation = 'slide-out ease 1s';
+    setTimeout(() => {
+        full.style.transform = 'translateX(100%)';
+    }, 100)
+});
+
+home.addEventListener('click', () => {
+    close.click();
+});
+projects.addEventListener('click', () => {
+    close.click();
+});
+about.addEventListener('click', () => {
+    close.click();
+});
+contact.addEventListener('click', () => {
+    close.click();
+});
+
+
 
 // Navigation Animation
 setInterval (() => {
@@ -18,6 +61,8 @@ setInterval (() => {
         nav_menu.style.height = '50px';
     }
 }, 1);
+
+
 
 //Project Card Animations
 setInterval (() => {
